90e2169b8e3da4e4956e326c5c7601caf2d9de7b
Mykuta Rakhmanyi <nikita.rahmany@gmail.com>
2023-12-20 12:49:26 +0100

    changed path and index.html

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-  createHashRouter,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home/Home";
 import SignUp from "./pages/SignUp/SignUp";
@@ -14,40 +10,35 @@ import SignIn from "./pages/SignIn/SignIn";
 import Profile from "./pages/Profile/Profile";
 import Course from "./pages/Course/Course";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/learnsphere/",
     element: <Layout />,
-    basename: "/learnsphere",
     // errorElement: <ErrorPage />,
     // loader: rootLoader,
     // action: rootAction,
     children: [
       // errorElement: <ErrorPage />,
       { index: true, element: <Home /> },
-      // {
-      //   path: "/learnsphere/",
-      //   element: <Home />,
-      // },
       {
-        path: "sign-up",
+        path: "/learnsphere/sign-up",
         element: <SignUp />,
       },
       {
-        path: "sign-in",
+        path: "/learnsphere/sign-in",
         element: <SignIn />,
       },
 
       {
-        path: "profile/student",
+        path: "/learnsphere/profile/student",
         element: <Profile />,
       },
       {
-        path: "profile/teacher",
+        path: "/learnsphere/profile/teacher",
         element: <Profile />,
       },
       {
-        path: "course",
+        path: "/learnsphere/course",
         element: <Course />,
       },
     ],
